cbf42431d6738fd5fdcc4bc4f547937b5d7185ab
abhishek-rai-1 <abhishekrai1972@gmail.com>
2025-06-06 10:16:09 +0530

    2_hard

--- Patterns/Hard/2_hard.cpp
+++ Patterns/Hard/2_hard.cpp
@@ -7,4 +7,15 @@ int main(){
     cout<<"Enter the number: ";
     cin>>n;
 
+    for(int row = 1; row <= n; row++){
+
+        // spaces
+        for(int space = 1; space <= n - row; space++)
+            cout<<"  ";
+
+        // numbers
+        for(int num = 1; num <= row; num++)
+            cout<<row <<" ";
+        cout<<endl;
+    }
 }
