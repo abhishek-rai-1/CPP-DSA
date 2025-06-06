b9b8dd21e2335799f25abe4505488653b182129c
abhishek-rai-1 <abhishekrai1972@gmail.com>
2025-06-06 10:19:15 +0530

    3_hard

---
+++ Patterns/Hard/3_hard.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+
+    cout<<"Enter the number: ";
+    cin>>n;
+
+    for(int row = 1; row <= n; row++){
+        // spaces
+        for(int space = 1; space <= n - row; space++)
+            cout<<"  ";
+        // numbers
+        for(int num = 1; num <= row; num++)
+            cout<<num<<" ";
+        cout<<endl;
+    }
+}
